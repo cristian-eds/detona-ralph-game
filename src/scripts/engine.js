a96aa94982f32df46d1bc960f54309ebff411ae8
Cristian <cristian.santos0308@gmail.com>
2024-10-03 15:13:21 -0300

    Criado funcao para diminuir tempo restante de jogo, atualizando state e valor na tela

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -10,6 +10,21 @@ const state = {
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        countDownTimerId: setInterval(countDown,1000),
+    }
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.values.timerId);
+        alert("Game Over! O seu resultado foi: "+ state.values.result)
     }
 }
 
