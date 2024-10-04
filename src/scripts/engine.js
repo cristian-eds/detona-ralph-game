04fb178515ba3445a657b38adebeccf92ce5ba51
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-10-03 21:52:14 -0300

    Adicionado botao reiniciar game, e funcoes para resetar estados

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,7 +4,8 @@ const state = {
         enemy: document.querySelector(".enemy"),
         timeLeft: document.getElementById("time-left"),
         score: document.getElementById("score"),
-        lives: document.getElementById("lives")
+        lives: document.getElementById("lives"),
+        btnRestart: document.querySelector(".btn-restart")
     },
     values: {
         timerId: null,
@@ -17,6 +18,20 @@ const state = {
     actions: {
         countDownTimerId: setInterval(countDown,1000),
     }
+}
+
+function restartGame() {
+    state.values.currentTime = 30;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    state.values.result = 0;
+    state.view.score.textContent = state.values.result;
+
+    state.values.remainingLives = 3;
+    state.view.lives.textContent = state.values.remainingLives;
+
+    state.actions.countDownTimerId = setInterval(countDown,1000);
+    moveEnemy();
 }
 
 function countDown() {
@@ -37,6 +52,14 @@ function livesDown() {
     }
 }
 
+function showButtonRestart() {
+    state.view.btnRestart.addEventListener('mousedown',() => {
+        restartGame();
+        state.view.btnRestart.classList.add("hide");
+    })
+    state.view.btnRestart.classList.remove("hide");
+}
+
 function endGame() {
     state.values.currentTime = 0;
     state.view.timeLeft.textContent = state.values.currentTime;
@@ -44,6 +67,7 @@ function endGame() {
     clearInterval(state.actions.countDownTimerId);
     clearInterval(state.values.timerId);
     alert("Game Over! O seu resultado foi: "+ state.values.result);
+    showButtonRestart();
 }
 
 function playSound(){
